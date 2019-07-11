189832a8c25ad4facf8f3e582ed4ef078eda8acb
Siyu Zhou <siyu.zhou@outlook.com>
2019-07-11 15:08:13 -0700

    Change style of arguments.

--- rlmodels/networks/encoders.py
+++ rlmodels/networks/encoders.py
@@ -3,7 +3,7 @@ from tensorflow import keras
 
 
 class NoEncoder(keras.layers.Layer):
-    def __init__(self, input_shape, params):
+    def __init__(self, input_shape):
         super().__init__()
         self.layer = keras.layers.Lambda(lambda x: x, input_shape=input_shape)
 
@@ -12,16 +12,16 @@ class NoEncoder(keras.layers.Layer):
 
 
 class MLPEncoder(keras.layers.Layer):
-    def __init__(self, input_shape, params):
-        if not params['hidden_units']:
+    def __init__(self, input_shape, hidden_units):
+        if not hidden_units:
             raise ValueError('hidden_units cannot be empty')
-        hidden_units = params['hidden_units']
 
         super().__init__()
 
         name = f'dense_{0}'
         self.hidden_layers = [name]
-        setattr(self, name, keras.layers.Dense(hidden_units[0], input_shape=input_shape, activation='relu'))
+        setattr(self, name, keras.layers.Dense(
+            hidden_units[0], input_shape=input_shape, activation='relu'))
         for i, units in enumerate(hidden_units[1:]):
             name = f'dense_{i+1}'
             self.hidden_layers.append(name)
@@ -36,17 +36,17 @@ class MLPEncoder(keras.layers.Layer):
 
 
 class CNNEncoder(keras.layers.Layer):
-    def __init__(self, input_shape, params):
-        if not params['filters']:
+    def __init__(self, input_shape, filters):
+        if filters:
             raise ValueError('filters cannot be empty')
-        filters = params['filters']
 
         super().__init__()
 
         conv_name = f'conv_{0}'
         pool_name = f'pool_{0}'
         self.hidden_layers = [conv_name, pool_name]
-        setattr(self, conv_name, keras.layers.Conv2D(filters[0], input_shape=input_shape, activation='relu'))
+        setattr(self, conv_name, keras.layers.Conv2D(
+            filters[0], input_shape=input_shape, activation='relu'))
         setattr(self, pool_name, keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
         for i, channels in enumerate(filters[1:]):
